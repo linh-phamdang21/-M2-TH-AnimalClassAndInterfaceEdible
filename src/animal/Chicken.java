6f7b186089eb673aea86b36bf522cfaf2198af75
linh-phamdang21 <ph.danglinh214@gmail.com>
2020-03-27 18:23:43 +0700

    Add files

---
+++ src/animal/Chicken.java
@@ -0,0 +1,14 @@
+package animal;
+
+import edible.Edible;
+
+public class Chicken extends Animal implements Edible {
+    @Override
+    public String makeSound() {
+        return "Chicken: cluck-cluck!";
+    }
+
+    public String howToEat(){
+        return "Could be fried";
+    }
+}
